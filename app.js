1190a250de4b8e1c0528b00bd82395c3416f7f91
mohmmad.rahgoshahi <mamad.rahgoshahi@gmail.com>
2023-01-22 23:24:06 +0330

    create_menu&imgProfile

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ console.log(screen);
    
 });
 
-console.log("tetgeg");
 let elm_menu = document.querySelector("#btnMenu");
+
+console.log(elm_menu);
 elm_menu.addEventListener('click',clickMenu)
 function clickMenu(){
     console.log("teatClickMenu");
@@ -14,21 +15,14 @@ let elmNavHeader= document.querySelector("#navHeader");
 let elmHeader = document.querySelector("header")
 console.log(elmNavHeader);
 elmHeader.insertAdjacentHTML("beforebegin",`
-<nav id="navHeader" class="flex justify-between">
-<div class=" w-15 h-15 p-2  bg-blue-300/25  rounded-md">dark mode</div>
- <div class="w-10 h-10 rounded-full bg-gray-500"></div>
-<div class="w-10 h-10 rounded-full bg-gray-500"></div>
-<div class="w-10 h-10 rounded-full bg-gray-500"></div>
-<div class="w-10 h-10 rounded-full bg-gray-500"></div>
-<div class="w-10 h-10 rounded-full bg-gray-500"></div>
-<div class="w-10 h-10 rounded-full bg-gray-500"></div>
-
-</nav>
+      <div class="w-10 h-10 fixed top-28 left-4 rounded-full bg-gray-500"></div>
+      <div class="w-10 h-10 fixed top-24 left-16  rounded-full bg-gray-500"></div>
+      <div class="w-10 h-10 fixed top-16 left-28  rounded-full bg-gray-500"></div>
+      <div class="w-10 h-10 fixed top-4 left-36 rounded-full bg-gray-500"></div>
 `)
-
-
 }
 // elm_menu.onclick=()=>{
+//     console.log("test");
 // // let ulElmMenu = document.createElement("ul");
 // elm_nav_header.insertAdjacentElement("beforeend",`
 // <div class=" w-15 h-15 p-2  bg-blue-300/25  rounded-md">dark mode</div>
