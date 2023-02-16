a87f4544f61e9121d37b190080f2e3217196b1b7
mohmmad.rahgoshahi <mamad.rahgoshahi@gmail.com>
2023-02-16 15:17:51 +0330

    newAnmiaton

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-console.log(anime);
-document.addEventListener('load', (event) => {
- console.log(screen.height);
-console.log(screen.width);
-console.log(screen);
-   
-});
-
+//! menuClick
 let elm_menu = document.querySelector("#btnMenu");
-
-
 let isOffMenu = true;
-elm_menu.addEventListener('click',clickMenu)
+elm_menu.addEventListener('click',clickMenu);
 function clickMenu(){
     if(isOffMenu===true){
-isOffMenu=false;
+     isOffMenu=false;
     console.log("click");
 let elmNavHeader= document.querySelector("#navHeader");
-let elmHeader = document.querySelector("header")
+let elmHeader= document.querySelector("header");
 elmHeader.insertAdjacentHTML("beforebegin",`
-      <div id="navMenu">
-      <div class="w-10 h-10 fixed top-28 left-4 rounded-full bg-gray-500"></div>
-      <div class="w-10 h-10 fixed top-24 left-16  rounded-full bg-gray-500"></div>
-      <div class="w-10 h-10 fixed top-16 left-28  rounded-full bg-gray-500"></div>
-      <div class="w-10 h-10 fixed top-4 left-36 rounded-full bg-gray-500"></div>
+<div id="navMenu">
+<a href="#mySkill" class="w-10 h-10 fixed top-28 left-4 rounded-full bg-gray-500">skill</a>
+<div class="w-10 h-10 fixed top-24 left-16  rounded-full bg-gray-500"></div>
+<div class="w-10 h-10 fixed top-16 left-28  rounded-full bg-gray-500"></div>
+<div class="w-10 h-10 fixed top-4 left-36 rounded-full bg-gray-500"></div>
 </div>
+
 `)
     }else{
         let menuFix =document.querySelector("#navMenu");
@@ -33,8 +25,61 @@ elmHeader.insertAdjacentHTML("beforebegin",`
         console.log("click2");
     }
 
+//! menuClickTheEnd
     
 }
+//!MySkill&AboutMe
+//winScroll()
+  // if(window.screenY > 196){
+  //   console.log("test");
+
+
+  // }
+// window.addEventListener("scroll",(event)=>{
+
+//  if(window.scrollY >= 678){
+// // window.scrollBy(0,10)
+// // let elmMySkill = document.querySelector("#mySkill");
+// console.log(elmMySkill);
+//  }
+// });
+var mySkillElm = document.querySelector('#mySkill')
+var myAboutElm = document.querySelector('#AboutMe')
+window.onscroll=()=>{
+  winScroll(678,mySkillElm,"animate__backInLeft");
+  winScroll(678,myAboutElm,"animate__backInRight");
+  // if (window.scrollY > 678) {
+  //   mySkillElm.classList.add("animate__backInLeft");
+  // } else {
+  //   mySkillElm.classList.remove("animate__backInLeft");
+  // }
+}
+
+function winScroll(number,element,classId) {
+  if (window.scrollY > number) {
+    element.classList.add(classId);
+  } else {
+    element.classList.remove(classId);
+  }
+}
+// function scrollPart(numScroll){
+//   console.log(window.scrollY);
+//   if(window.scrollY >= numScroll){
+//     console.log("test2");
+//    }
+//  else{
+//     console.log("test3");
+//   }
+// }
+
+ 
+// function winScroll(number,element,classId) {
+//   if (window.scrollY > number) {
+//     element.classList.add(classId);
+//   } else {
+//     element.classList.remove(classId);
+//   }
+// }
 let titleSite = document.querySelector(".titleSite");
 let project1El = document.querySelector(".project1");
 project1El.addEventListener("mouseenter",()=>{
@@ -75,7 +120,6 @@ anime.timeline({loop: true})
     delay: 1000
   });
 
-console.log(project1El);
 // elm_menu.onclick=()=>{
 //     console.log("test");
 // // let ulElmMenu = document.createElement("ul");
